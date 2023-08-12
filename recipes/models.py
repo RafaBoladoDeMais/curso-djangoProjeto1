57677bb0734225929320fe52027bcf5096d5f71c
RafaBoladoDeMais <rafael_verao@ufms.br>
2023-08-12 11:29:01 -0300

    Some atualizations: models, views, etc...

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Recipe(models.Model):
+    class Meta:
+        verbose_name = 'Recipe'
+        verbose_name_plural = 'Recipes'
+
+    title = models.CharField(max_length=65)
+    decription = models.CharField(max_length=165)
+    slug = models.SlugField()
+    preparation_time = models.CharField(max_length=65)
+    preparation_time_unit = models.IntegerField()
+    servings = models.CharField(max_length=65)
+    servings_unit = models.IntegerField()
+    prepartaion_steps = models.TextField()
+    prepartaion_steps_is_html = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updatad_at = models.DateTimeField(auto_created=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d')
+
+    
+    def __str__(self):
+        return self.title
--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils.recipes import factory
 # Create your views here.
 
 def home(request):
+    recipe = [factory.make_recipe() for _ in range(10)]
+    context ={
+        'page_title': 'home',
+        'recipes': recipe,
+    }
+    return render(request, 'recipes/pages/home.html', context)
 
-    return render(request, 'recipes/pages/home.html')
+def recipes(request, id):
+
+    context ={
+        'page_title': 'recipe',
+        'recipe': factory.make_recipe(),
+        'one_page': True,
+    }
+
+    return render(request, 'recipes/pages/recipes_page.html', context)
