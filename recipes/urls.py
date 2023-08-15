f057292f5f16abe3d6e1b5d81e17a185bcf433b1
RafaBoladoDeMais <rafael_verao@ufms.br>
2023-08-15 08:40:56 -0300

    Some migrations and pytest.ini created

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,4 +5,5 @@ app_name = 'recipes'
 urlpatterns = [
     path('', views.home, name='home'), 
     path('recipes/<int:id>/', views.recipes, name='recipe'), 
+    path('recipes/category/<int:id>/', views.category, name='category'), 
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,22 +1,45 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.http import Http404
 from utils.recipes import factory
-# Create your views here.
+from . import models
+
+# Create your views here. 
 
 def home(request):
-    recipe = [factory.make_recipe() for _ in range(10)]
+    recipe = models.Recipe.objects.all().filter(is_published=True).order_by('-id')
     context ={
-        'page_title': 'home',
+        'page_title': 'Home',
         'recipes': recipe,
     }
     return render(request, 'recipes/pages/home.html', context)
 
 def recipes(request, id):
+    recipe = models.Recipe.objects.filter(id=id).first()
+    recipe = get_object_or_404(models.Recipe.objects.filter(id=id, is_published=True))
+    page_title = f'Receita - {recipe.title}' #type: ignore 
 
     context ={
-        'page_title': 'recipe',
-        'recipe': factory.make_recipe(),
+        'page_title': page_title,
+        'recipe': recipe,
         'one_page': True,
     }
 
     return render(request, 'recipes/pages/recipes_page.html', context)
+
+def category(request, id):
+    # recipe = models.Recipe.objects.filter(category__id=id, is_published=True)
+    # if not recipe:
+    #     raise Http404('Not found 🙁')
+    recipe = get_list_or_404(
+        models.Recipe.objects.filter(
+        category__id=id, 
+        is_published=True).order_by('-id'),
+    )
+    
+    page_title = f'Categorias - {recipe[0].category.name}'#type: ignore 
+    context ={
+        'page_title': page_title,
+        'recipes': recipe,
+    }
+
+    return render(request, 'recipes/pages/home.html', context)
